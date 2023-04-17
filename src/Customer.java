eacc54bfebf0f9de4566e9341f05730585229115
prayatnaaa <tudeprayatna04@gmail.com>
2023-04-17 16:53:45 +0800

    fix class Customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,26 +1,14 @@
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Customer {
 
    private int menu;
-   private int jumlahMenu;
 
    private int jarak;
 
-   Customer(int menu, int jumlahMenu){
+    public void setMenu(int menu){
        this.menu=menu;
-       this.jumlahMenu=jumlahMenu;
-
-   }
-
+    }
    public int getMenu(){
        return menu;
-   }
-
-   public int getJumlahMenu(){
-       return jumlahMenu;
    }
 
    public void setJarak(int jarak){
