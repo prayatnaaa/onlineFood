4d1d763ea475747f6d9a0e6df720a45cfedc08ee
prayatnaaa <tudeprayatna04@gmail.com>
2023-04-17 16:55:39 +0800

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
+    Scanner sc=new Scanner(System.in);
+
+    static Admin admin = new Admin();
+
+    static void login(){
+        Scanner sc = new Scanner(System.in);
+
+        menuCustomer mc = new menuCustomer();
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+
+        if (username.equals("admin") && password.equals("admin")) {
+            admin.menuAdmin();
+        }
+        else{
+            ArrayList<Resto>restos=new ArrayList<>();
+            mc.loginCust();
+        }
+    }
     public static void main(String[] args) {
+        login();
+    }
 
-        Admin admin1=new Admin();
-
-        admin1.display();
-        admin1.register();
+    public static Admin getAdminObject(){
+        return admin;
     }
 }
+
